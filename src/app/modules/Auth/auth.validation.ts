e3bf91f402b1b2fdf3ed67fff0e2d9809eafaf42
SadikRa <sadikrahman494@gmail.com>
2025-05-05 00:48:44 +0600

    updated

--- src/app/modules/Auth/auth.validation.ts
+++ src/app/modules/Auth/auth.validation.ts
@@ -1,32 +1,44 @@
 import { z } from "zod";
 
 const registerUser = z.object({
-  email: z.string().email(),
-  password: z.string().min(6),
-  name: z.string(),
+  body: z.object({
+    email: z.string().email(),
+    password: z.string().min(6),
+    name: z.string(),
+  }),
 });
 
 const loginUser = z.object({
-  email: z.string().email(),
-  password: z.string().min(6),
+  body: z.object({
+    email: z.string().email(),
+    password: z.string().min(6),
+  }),
 });
 
 const getMyProfile = z.object({
-  email: z.string().email(),
+  body: z.object({
+    email: z.string().email(),
+  }),
 });
 
 const changePassword = z.object({
-  oldPassword: z.string().min(6),
-  newPassword: z.string().min(6),
+  body: z.object({
+    oldPassword: z.string().min(6),
+    newPassword: z.string().min(6),
+  }),
 });
 
 const forgotPassword = z.object({
-  email: z.string().email(),
+  body: z.object({
+    email: z.string().email(),
+  }),
 });
 const resetPassword = z.object({
-  token: z.string(),
-  newPassword: z.string(),
-  email: z.string(),
+  body: z.object({
+    token: z.string(),
+    newPassword: z.string(),
+    email: z.string(),
+  }),
 });
 export const AuthValidation = {
   registerUser,
