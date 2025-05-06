35059a522bd4dee9b8abe7023c41a44273b6bbf9
SadikRa <sadikrahman494@gmail.com>
2025-05-06 23:26:37 +0600

    vote CURD done

---
+++ src/app/modules/Vote/vote.service.ts
@@ -0,0 +1,77 @@
+import httpStatus from "http-status";
+import { Request } from "express";
+import prisma from "../../../shared/prisma";
+import AppError from "../../errors/AppError";
+import { Vote } from "@prisma/client";
+
+const createVote = async (req: Request) => {
+  const { reviewId, accountId } = req.body;
+
+  const isReviewExist = await prisma.review.findUnique({
+    where: { id: reviewId },
+  });
+
+  if (!isReviewExist) {
+    throw new AppError(httpStatus.NOT_FOUND, "Review not found");
+  }
+
+  const isAccountExist = await prisma.account.findUnique({
+    where: { id: accountId },
+  });
+
+  if (!isAccountExist) {
+    throw new AppError(httpStatus.NOT_FOUND, "Account not found");
+  }
+
+  const vote = await prisma.vote.create({
+    data: req.body,
+  });
+
+  return vote;
+};
+
+const getVotes = async () => {
+  const result = await prisma.vote.findMany();
+  return result;
+};
+
+const getAVote = async (id: string) => {
+  const result = await prisma.vote.findUniqueOrThrow({
+    where: { id },
+  });
+
+  return result;
+};
+
+const updateVote = async (id: string, data: Partial<Vote>) => {
+  await prisma.vote.findUniqueOrThrow({
+    where: { id },
+  });
+
+  const result = await prisma.vote.update({
+    where: { id },
+    data,
+  });
+
+  return result;
+};
+
+const deleteVote = async (id: string) => {
+  await prisma.vote.findUniqueOrThrow({
+    where: { id },
+  });
+
+  const result = await prisma.vote.delete({
+    where: { id },
+  });
+
+  return result;
+};
+
+export const VoteService = {
+  createVote,
+  getVotes,
+  getAVote,
+  updateVote,
+  deleteVote,
+};
